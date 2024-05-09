d9a01c6654c159ba2bc8d917876b98f3d3a9ce63
Davidcapera <126677035+Davidcapera@users.noreply.github.com>
2024-05-09 18:50:05 -0500

    creacion rama desarrollo

--- src/main/java/com/DogWalker/dog/walker/controlador/HomeController.java
+++ src/main/java/com/DogWalker/dog/walker/controlador/HomeController.java
@@ -11,7 +11,6 @@ import com.DogWalker.dog.walker.servicio.UsuarioService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
